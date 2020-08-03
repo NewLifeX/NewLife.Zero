cacf530cd656ba81fcddd18c012703e0fc6c4000
大石头 <nnhy@vip.qq.com>
2020-08-04 07:10:14 +0800

    简易型埋点，测量调用次数和耗时，跟内部HttpClient和数据库操作形成上下级调用链，并送往星尘监控中心

--- Zero.Agent/Program.cs
+++ Zero.Agent/Program.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using NewLife;
 using NewLife.Agent;
 using NewLife.Log;
+using NewLife.Remoting;
 using NewLife.Threading;
+using Stardust.Monitors;
+using XCode.DataAccessLayer;
 
 namespace Zero.Agent
 {
@@ -14,6 +18,8 @@ namespace Zero.Agent
     public class MyServices : ServiceBase
     {
         #region 属性
+        /// <summary>性能跟踪器</summary>
+        public ITracer Tracer { get; set; }
         #endregion
 
         #region 构造函数
@@ -37,20 +43,59 @@ namespace Zero.Agent
         {
             WriteLog("业务开始……");
 
+            // 配置APM性能跟踪器
+            var set = Setting.Current;
+            if (!set.TracerServer.IsNullOrEmpty())
+            {
+                // 配置指向星尘监控中心
+                var tracer = new StarTracer(set.TracerServer) { Log = XTrace.Log };
+                DefaultTracer.Instance = tracer;
+                ApiHelper.Tracer = tracer;
+                DAL.GlobalTracer = tracer;
+                Tracer = tracer;
+            }
+
             // 5秒开始，每60秒执行一次
-            _timer = new TimerX(DoWork, null, 5_000, 60_000) { Async = true };
+            _timer = new TimerX(DoWork1, null, 5_000, 60_000) { Async = true };
             // 每天凌晨2点13分执行一次
-            _timer2 = new TimerX(DoWork, null, DateTime.Today.AddMinutes(2 * 60 + 13), 24 * 3600 * 1000) { Async = true };
+            _timer2 = new TimerX(DoWork2, null, DateTime.Today.AddMinutes(2 * 60 + 13), 24 * 3600 * 1000) { Async = true };
 
             base.StartWork(reason);
         }
 
-        private void DoWork(Object state)
+        private void DoWork1(Object state)
         {
+            // 简易型埋点，测量调用次数和耗时，跟内部HttpClient和数据库操作形成上下级调用链，并送往星尘监控中心
+            using var span = Tracer?.NewSpan("work1");
+
             var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
 
             // 日志会输出到当前目录的Log子目录中
             XTrace.WriteLine($"代码执行时间：{time}");
+        }
+
+        private void DoWork2(Object state)
+        {
+            // 完整型埋点，增加测量错误次数和明细
+            using var span = Tracer?.NewSpan("work2");
+
+            var time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+            try
+            {
+                // 日志会输出到当前目录的Log子目录中
+                XTrace.WriteLine($"代码执行时间：{time}");
+            }
+            catch (Exception ex)
+            {
+                // 记录错误以及上下文，一起送往星尘监控中心
+                span?.SetError(ex, time);
+
+                throw;
+            }
+            finally
+            {
+                span?.Dispose();
+            }
         }
 
         /// <summary>停止服务</summary>
--- Zero.Agent/Setting.cs
+++ Zero.Agent/Setting.cs
@@ -2,7 +2,7 @@
 using System.ComponentModel;
 using NewLife.Configuration;
 
-namespace Zero.Web
+namespace Zero.Agent
 {
     /// <summary>配置</summary>
     [Config("Stardust")]
@@ -13,25 +13,9 @@ namespace Zero.Web
         [Description("调试开关。默认true")]
         public Boolean Debug { get; set; } = true;
 
-        /// <summary>服务端口。默认6600</summary>
-        [Description("服务端口。默认6600")]
-        public Int32 Port { get; set; } = 6600;
-
-        /// <summary>令牌有效期。默认2*3600秒</summary>
-        [Description("令牌有效期。默认2*3600秒")]
-        public Int32 TokenExpire { get; set; } = 2 * 3600;
-
-        /// <summary>会话超时。默认600秒</summary>
-        [Description("会话超时。默认600秒")]
-        public Int32 SessionTimeout { get; set; } = 600;
-
-        /// <summary>自动注册。允许客户端自动注册，默认true</summary>
-        [Description("自动注册。允许客户端自动注册，默认true")]
-        public Boolean AutoRegister { get; set; } = true;
-
         /// <summary>APM监控服务器。</summary>
         [Description("APM监控服务器。")]
-        public String TracerServer { get; set; }
+        public String TracerServer { get; set; } = "http://star.newlifex.com:6600";
         #endregion
 
         #region 构造
