0642de9af04a85bbd3eeb205b2f5274ceed50f11
笑笑 <xxred0324@qq.com>
2020-09-27 10:24:51 +0800

    update Zero.Web/Startup.cs. 暂时添加使用静态文件中间件

--- Zero.Web/Startup.cs
+++ Zero.Web/Startup.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -39,6 +39,7 @@ namespace Zero.Web
             }
 
             // 引入魔方
+            app.UseStaticFiles(); // 暂时添加，待更新新版本Cube后可删除
             services.AddCube();
 
             services.AddControllersWithViews();
