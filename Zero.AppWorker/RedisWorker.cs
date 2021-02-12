3974061c7d02d35157a40f304967022c09c9f405
大石头 <nnhy@vip.qq.com>
2021-02-12 11:53:02 +0800

    新增AppWorker，应用后台任务，借助轻量级应用主机App，处理非Web的后台任务型应用

---
+++ Zero.AppWorker/RedisWorker.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NewLife.Caching;
+using NewLife.Log;
+using NewLife.Model;
+using Zero.AppWorker.Models;
+
+namespace Zero.AppWorker
+{
+    public class RedisWorker : IHostedService
+    {
+        private readonly FullRedis _redis;
+        private readonly ILog _log;
+        private RedisReliableQueue<String> _queue;
+
+        public RedisWorker(FullRedis redis, ILog log)
+        {
+            _redis = redis;
+            _log = log;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var task = ExecuteAsync(cancellationToken);
+            return task.IsCompleted ? task : Task.CompletedTask;
+        }
+
+        protected async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            await Task.Yield();
+
+            // Redis 可信消息队列，支持消费确认
+            _queue = _redis.GetReliableQueue<String>("rdsTopic");
+
+            await _queue.ConsumeAsync<Area>(area =>
+            {
+                XTrace.WriteLine("RedisQueue.Consume {0} {1}", area.Code, area.Name);
+            }, stoppingToken, _log);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ Zero.AppWorker/RocketMqWorker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using NewLife;
+using NewLife.Log;
+using NewLife.Model;
+using NewLife.RocketMQ;
+
+namespace Zero.AppWorker
+{
+    public class RocketMqWorker : IHostedService
+    {
+        private readonly Consumer _consumer;
+
+        public RocketMqWorker(Consumer consumer) => _consumer = consumer;
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+
+            _consumer.OnConsume = (q, ms) =>
+            {
+                XTrace.WriteLine("[{0}@{1}]收到消息[{2}]", q.BrokerName, q.QueueId, ms.Length);
+
+                foreach (var item in ms.ToList())
+                {
+                    XTrace.WriteLine($"消息：主键【{item.Keys}】，产生时间【{item.BornTimestamp.ToDateTime()}】，内容【{item.Body.ToStr()}】");
+                }
+
+                return true;
+            };
+
+            _consumer.Start();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
