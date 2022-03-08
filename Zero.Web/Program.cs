3cbd29ec516ed42d5b15cc3011cd93f355883d51
大石头 <nnhy@vip.qq.com>
2022-03-08 14:00:26 +0800

    NET6模版

--- Zero.Web/Program.cs
+++ Zero.Web/Program.cs
@@ -1,44 +1,62 @@
-﻿using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using NewLife;
+using NewLife.Caching;
+using NewLife.Cube;
 using NewLife.Log;
-using Zero.Data.Projects;
+using XCode;
 
-namespace Zero.Web
+//!!! 标准Web项目模板，新生命团队强烈推荐
+
+// 启用控制台日志，拦截所有异常
+XTrace.UseConsole();
+
+var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+
+// 配置星尘。借助StarAgent，或者读取配置文件 config/star.config 中的服务器地址
+var star = services.AddStardust(null);
+
+// 启用星尘配置中心。分布式部署或容器化部署推荐使用，单机部署不推荐使用
+var config = star.Config;
+
+// 默认内存缓存，如有配置可使用Redis缓存
+var cache = new MemoryCache();
+if (config != null && !config["redisCache"].IsNullOrEmpty())
+    services.AddSingleton<ICache>(p => new FullRedis(p, "redisCache") { Name = "Cache", Tracer = star.Tracer });
+else
+    services.AddSingleton<ICache>(cache);
+
+services.AddControllersWithViews();
+
+// 引入魔方
+services.AddCube();
+
+var app = builder.Build();
+
+// 预热数据层，执行反向工程建表等操作
+EntityFactory.InitConnection("Membership");
+EntityFactory.InitConnection("Log");
+EntityFactory.InitConnection("Cube");
+EntityFactory.InitConnection("Zero");
+
+// 使用Cube前添加自己的管道
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseExceptionHandler("/CubeHome/Error");
+
+// 使用魔方
+app.UseCube(app.Environment);
+
+app.UseAuthorization();
+
+// 启用星尘注册中心，向注册中心注册服务，服务消费者将自动更新服务端地址列表
+app.RegisterService("Zero.Web", null, "dev");
+
+app.UseEndpoints(endpoints =>
 {
-    public class Program
-    {
-        public static void Main(String[] args)
-        {
-            // 启用控制台日志，拦截所有异常
-            XTrace.UseConsole();
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=CubeHome}/{action=Index}/{id?}");
+});
 
-            // 异步初始化
-            Task.Run(InitAsync);
-
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(String[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-
-        private static void InitAsync()
-        {
-            // 配置
-            var set = NewLife.Setting.Current;
-            if (set.IsNew)
-            {
-                set.DataPath = "../Data";
-                set.Save();
-            }
-
-            // 初始化数据库
-            var n = Team.Meta.Count;
-        }
-    }
-}
+app.Run();
