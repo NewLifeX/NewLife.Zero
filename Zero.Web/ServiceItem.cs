06170523db6b55766a7ec2afd11fdfdac954cefe
大石头 <nnhy@vip.qq.com>
2022-05-25 16:41:03 +0800

    增加配置文件模版

---
+++ Zero.Web/ServiceItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Xml.Serialization;
+
+namespace Zero.Web;
+
+/// <summary>应用服务信息</summary>
+public class ServiceItem
+{
+    #region 属性
+    /// <summary>名称。全局唯一，默认应用名</summary>
+    [XmlAttribute]
+    public String Name { get; set; }
+
+    /// <summary>Url地址</summary>
+    [XmlAttribute]
+    public String Url { get; set; }
+
+    /// <summary>延迟时间。默认0ms</summary>
+    [XmlAttribute]
+    public Int32 Delay { get; set; }
+    #endregion
+}
---
+++ Zero.WebApi/ProxySetting.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel;
+using NewLife.Configuration;
+
+namespace Zero.WebApi;
+
+/// <summary>代理设置</summary>
+[DisplayName("代理设置")]
+[Config("Proxy")]
+public class ProxySetting : Config<ProxySetting>
+{
+    #region 属性
+    /// <summary>调试。默认启用</summary>
+    [Description("调试。默认启用")]
+    public Boolean Debug { get; set; } = true;
+
+    /// <summary>端口。默认1080</summary>
+    [Description("端口。默认1080")]
+    public Int32 Port { get; set; } = 1080;
+
+    /// <summary>服务集合</summary>
+    [Description("服务集合")]
+    public ServiceItem[] Services { get; set; }
+    #endregion
+
+    #region 方法
+    /// <summary>加载完成后</summary>
+    protected override void OnLoaded()
+    {
+        if (Port == 0) Port = 1080;
+
+        if (Services == null || Services.Length == 0)
+        {
+            var si = new ServiceItem
+            {
+                Name = "test",
+                Url = "http://127.0.0.1:8000",
+                Delay = 0,
+            };
+            var si2 = new ServiceItem
+            {
+                Name = "test2",
+                Url = "http://127.0.0.1:8100",
+                Delay = 100,
+            };
+
+            Services = new[] { si, si2 };
+        }
+
+        base.OnLoaded();
+    }
+    #endregion
+}
