b2b98db74c38421d357242bba9edbdd2a09094ed
大石头 <nnhy@vip.qq.com>
2022-11-06 12:44:32 +0800

    新增Worker版Agent项目模板

---
+++ Zero.Agent2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NewLife.Extensions.Hosting.AgentService;
+using NewLife.Log;
+using Zero.Agent2;
+
+XTrace.UseConsole();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddStardust();
+
+        services.AddHostedService<Worker>();
+    })
+    //.UseWindowsService(options =>
+    //{
+    //    options.ServiceName = "TestWorker";
+    //})
+    .UseAgentService(options =>
+    {
+        options.ServiceName = "ZeroAgent";
+        options.DisplayName = "Worker服务测试";
+        options.Description = "Worker服务的测试应用";
+    })
+    .Build();
+
+await host.RunAsync();
---
+++ Zero.Agent2/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using NewLife.Log;
+
+namespace Zero.Agent2;
+
+public class Worker : BackgroundService
+{
+    private readonly ILog _logger;
+
+    public Worker(ILog logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.Info("Worker running at: {0}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        XTrace.WriteLine(nameof(StartAsync));
+
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        XTrace.WriteLine(nameof(StopAsync));
+
+        return base.StopAsync(cancellationToken);
+    }
+}
