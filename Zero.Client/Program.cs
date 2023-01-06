071602cae5218f7b17a3fc27bef559c4d5852b5a
大石头 <nnhy@vip.qq.com>
2023-01-06 23:35:53 +0800

    CS架构的客户端应用，以后台服务工作，一般由StarAgent负责守护

---
+++ Zero.Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using NewLife;
+using NewLife.Log;
+using NewLife.Model;
+using NewLife.MQTT;
+using Stardust;
+
+// 启用控制台日志，拦截所有异常
+XTrace.UseConsole();
+
+// 初始化对象容器，提供注入能力
+var services = ObjectContainer.Current;
+services.AddSingleton(XTrace.Log);
+
+// 配置星尘。自动读取配置文件 config/star.config 中的服务器地址
+var star = new StarFactory();
+if (star.Server.IsNullOrEmpty()) star = null;
+
+// 初始化Redis、MQTT、RocketMQ，注册服务到容器
+InitMqtt(services, star?.Tracer);
+
+Thread.Sleep(-1);
+
+static void InitMqtt(IObjectContainer services, ITracer? tracer)
+{
+    // 引入 MQTT
+    var mqtt = new MqttClient
+    {
+        Tracer = tracer,
+        Log = XTrace.Log,
+
+        Server = "tcp://127.0.0.1:1883",
+        ClientId = Environment.MachineName,
+        UserName = "stone",
+        Password = "Pass@word",
+    };
+    services.AddSingleton(mqtt);
+}
