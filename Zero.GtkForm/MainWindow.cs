dac278ec1522c726aa2e7ee8a4182c05b8e47aaf
大石头 <nnhy@vip.qq.com>
2024-08-23 11:12:29 +0800

    新增GTK桌面应用模版项目，支持接入星尘

---
+++ Zero.GtkForm/MainWindow.cs
@@ -0,0 +1,33 @@
+﻿using Gtk;
+using UI = Gtk.Builder.ObjectAttribute;
+
+namespace Zero.GtkForm;
+
+internal class MainWindow : Window
+{
+    [UI] private Label _label1 = null;
+    [UI] private Button _button1 = null;
+
+    private Int32 _counter;
+
+    public MainWindow() : this(new Builder("MainWindow.glade")) { }
+
+    private MainWindow(Builder builder) : base(builder.GetRawOwnedObject("MainWindow"))
+    {
+        builder.Autoconnect(this);
+
+        DeleteEvent += Window_DeleteEvent;
+        _button1.Clicked += Button1_Clicked;
+    }
+
+    private void Window_DeleteEvent(Object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+    }
+
+    private void Button1_Clicked(Object sender, EventArgs a)
+    {
+        _counter++;
+        _label1.Text = "Hello World! This button has been clicked " + _counter + " time(s).";
+    }
+}
---
+++ Zero.GtkForm/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using Gtk;
+using NewLife;
+using NewLife.Log;
+using NewLife.Model;
+using Stardust;
+using XCode;
+using XCode.Membership;
+using Node = Zero.Data.Nodes.Node;
+
+namespace Zero.GtkForm;
+
+internal static class Program
+{
+    /// <summary>
+    ///  The main entry point for the application.
+    /// </summary>
+    [STAThread]
+    static void Main()
+    {
+        // 支持GB2312编码
+        Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+        XTrace.UseConsole();
+        MachineInfo.RegisterAsync();
+
+        StartClient();
+
+        GLib.ExceptionManager.UnhandledException += ExceptionManager_UnhandledException;
+
+        // 预热数据层，执行自动建表等操作
+        Task.Run(() =>
+        {
+            var dal = User.Meta.Session.Dal;
+            dal = Node.Meta.Session.Dal;
+            _ = EntityFactory.InitAllAsync();
+        });
+
+        Application.Init();
+
+        var app = new Application(_factory.AppId, GLib.ApplicationFlags.None);
+        app.Register(GLib.Cancellable.Current);
+
+        var win = new MainWindow();
+        app.AddWindow(win);
+
+        win.Show();
+        Application.Run();
+    }
+
+    private static void ExceptionManager_UnhandledException(GLib.UnhandledExceptionArgs args)
+    {
+        if (args.ExceptionObject is Exception ex) XTrace.WriteException(ex);
+    }
+
+    static StarFactory _factory;
+    static StarClient _Client;
+    private static void StartClient()
+    {
+        var set = ClientSetting.Current;
+        var server = set.Server;
+        if (server.IsNullOrEmpty()) return;
+
+        XTrace.WriteLine("初始化服务端地址：{0}", server);
+
+        _factory = new StarFactory(server, "ZeroGtk", null)
+        {
+            Log = XTrace.Log,
+        };
+
+        var client = new StarClient(server)
+        {
+            Code = set.Code,
+            Secret = set.Secret,
+            ProductCode = "ZeroGtk",
+            Setting = set,
+
+            Tracer = _factory.Tracer,
+            Log = XTrace.Log,
+        };
+
+        client.Open();
+
+        Host.RegisterExit(() => client.Logout("ApplicationExit"));
+
+        _Client = client;
+    }
+}
